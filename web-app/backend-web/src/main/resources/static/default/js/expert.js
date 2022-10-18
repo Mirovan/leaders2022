94c9ecd44ff9e58fcc2a3a2a6b3df72b03bd9736
milovanov.mm <milovanov.mm@esphere.ru>
2022-10-18 22:44:51 +0300

    Рефакторинг

--- web-app/backend-web/src/main/resources/static/default/js/expert.js
+++ web-app/backend-web/src/main/resources/static/default/js/expert.js
@@ -5,20 +5,20 @@ const map = new ol.Map({
         new ol.layer.Tile({
             source: new ol.source.OSM()
         }),
-        new ol.layer.Heatmap({
-            source: new ol.source.Vector({
-                url: '/kml',
-                format: new ol.format.KML({
-                    extractStyles: false,
-                }),
-            }),
-            blur: parseInt(10),
-            radius: parseInt(10),
-            weight: function (feature) {
-                const area = parseFloat(feature.get('area')) / 10000;
-                return area;
-            },
-        })
+        // new ol.layer.Heatmap({
+        //     source: new ol.source.Vector({
+        //         url: '/kml',
+        //         format: new ol.format.KML({
+        //             extractStyles: false,
+        //         }),
+        //     }),
+        //     blur: parseInt(10),
+        //     radius: parseInt(10),
+        //     weight: function (feature) {
+        //         const area = parseFloat(feature.get('area')) / 10000;
+        //         return area;
+        //     },
+        // })
     ],
     view: new ol.View({
         center: ol.proj.transform([37.646930, 55.725146], 'EPSG:4326', 'EPSG:3857'),
@@ -43,46 +43,19 @@ map.on('click', function (evt) {
 
     $.get("/malls/nearest", {latitude: lat, longitude: lon})
         .done(function (data) {
-            let tableData = "";
+            let tableData = "<tr><th colspan='3'>Торговый центры</th></tr>";
 
             for (let item in data) {
                 //console.log(JSON.stringify(data[item]));
 
-                tableData += "<tr>" +
-                    "<td>" + data[item]["address"] + "<br />(" + data[item]["name"] + ")" + "</td>" +
-                    "<td>" + data[item]["phone"] + "</td>" +
-                    "<td><button class=\"btn btn-outline-primary btn-sm\" onclick=\"savePostamat(" +
-                    "'" + data[item]["name"] + "', " +
-                    "'" + data[item]["address"] + "', " +
-                    data[item]["latitude"] + ", " +
-                    data[item]["longitude"] +
-                    ")\">Установить</button></td>"
-                "</tr>";
+                tableData += getInsertPostamatRow(
+                    data[item]["name"],
+                    data[item]["address"],
+                    data[item]["phone"],
+                    data[item]["lat"],
+                    data[item]["lon"]);
 
-                //Рисуем точки на карте - ближайшие объекты к нажатой точке
-                var centerLongitudeLatitude = ol.proj.fromLonLat([
-                    data[item]["longitude"], data[item]["latitude"]
-                ]);
-                var layer = new ol.layer.Vector({
-                    source: new ol.source.Vector({
-                        projection: 'EPSG:4326',
-                        features: [
-                            new ol.Feature(new ol.geom.Circle(centerLongitudeLatitude, 200))
-                        ]
-                    }),
-                    name: 'NEAREST_OBJECT',
-                    style: [
-                        new ol.style.Style({
-                            stroke: new ol.style.Stroke({
-                                color: 'green',
-                                width: 2
-                            }),
-                            fill: new ol.style.Fill({
-                                color: 'rgba(0, 0, 255, 0.1)'
-                            })
-                        })
-                    ]
-                });
+                var layer = createLayer('NEAREST_OBJECT', 'green', 200, data[item]["latitude"], data[item]["longitude"]);
                 map.addLayer(layer);
             }
 
@@ -92,46 +65,19 @@ map.on('click', function (evt) {
 
     $.get("/supermarkets/nearest", {latitude: lat, longitude: lon})
         .done(function (data) {
-            let tableData = "";
+            let tableData = "<tr><th colspan='3'>Супермаркеты</th></tr>";
 
             for (let item in data) {
                 //console.log(JSON.stringify(data[item]));
 
-                tableData += "<tr>" +
-                    "<td>" + data[item]["address"] + "<br />(" + data[item]["name"] + ")" + "</td>" +
-                    "<td>" + data[item]["phone"] + "</td>" +
-                    "<td><button class=\"btn btn-outline-primary btn-sm\" onclick=\"savePostamat(" +
-                    "'" + data[item]["name"] + "', " +
-                    "'" + data[item]["address"] + "', " +
-                    data[item]["latitude"] + ", " +
-                    data[item]["longitude"] +
-                    ")\">Установить</button></td>"
-                "</tr>";
+                tableData += getInsertPostamatRow(
+                    data[item]["name"],
+                    data[item]["address"],
+                    data[item]["phone"],
+                    data[item]["lat"],
+                    data[item]["lon"]);
 
-                //Рисуем точки на карте - ближайшие объекты к нажатой точке
-                var centerLongitudeLatitude = ol.proj.fromLonLat([
-                    data[item]["longitude"], data[item]["latitude"]
-                ]);
-                var layer = new ol.layer.Vector({
-                    source: new ol.source.Vector({
-                        projection: 'EPSG:4326',
-                        features: [
-                            new ol.Feature(new ol.geom.Circle(centerLongitudeLatitude, 200))
-                        ]
-                    }),
-                    name: 'NEAREST_OBJECT',
-                    style: [
-                        new ol.style.Style({
-                            stroke: new ol.style.Stroke({
-                                color: 'blue',
-                                width: 2
-                            }),
-                            fill: new ol.style.Fill({
-                                color: 'rgba(0, 0, 255, 0.1)'
-                            })
-                        })
-                    ]
-                });
+                var layer = createLayer('NEAREST_OBJECT', 'blue', 200, data[item]["latitude"], data[item]["longitude"]);
                 map.addLayer(layer);
             }
 
@@ -159,4 +105,49 @@ function clearMap(layerName) {
     map.getLayers().getArray()
         .filter(layer => layer.get('name') === layerName)
         .forEach(layer => map.removeLayer(layer));
+}
+
+function createLayer(layerName, color, square, lat, lon) {
+    //Рисуем точки на карте - ближайшие объекты к нажатой точке
+    var centerLongitudeLatitude = ol.proj.fromLonLat([
+        lon, lat
+    ]);
+
+    return new ol.layer.Vector({
+        source: new ol.source.Vector({
+            projection: 'EPSG:4326',
+            features: [
+                new ol.Feature(new ol.geom.Circle(centerLongitudeLatitude, square))
+            ]
+        }),
+        name: layerName,
+        style: [
+            new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                    color: color,
+                    width: 2
+                }),
+                fill: new ol.style.Fill({
+                    color: 'rgba(0, 0, 255, 0.1)'
+                })
+            })
+        ]
+    });
+}
+
+
+function getInsertPostamatRow(name, address, phone, lat, lon) {
+    address = address.replace("Российская Федерация, внутригородская территория", "").trim();
+    var res =
+    "<tr>" +
+        "<td>" + address + "<br />(" + name + ")" + "</td>" +
+        "<td>" + phone + "</td>" +
+        "<td><button class=\"btn btn-outline-primary btn-sm\" onclick=\"savePostamat(" +
+        "'" + name + "', " +
+        "'" + address + "', " +
+        lat + ", " +
+        lon +
+        ")\">Установить</button></td>"
+    "</tr>";
+    return res;
 }
